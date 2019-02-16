42d2a99cfc72d69bb3b22fc68bd7300ec01c075f
Reiner Czerwinski <rczerwi@web.de>
2019-02-16 09:36:01 +0100

    Load in DataFrame

--- readlog.py
+++ readlog.py
@@ -1,7 +1,11 @@
 import datetime
+import pandas as pd
 
 f = open("syslog","r")
 l = f.readlines()
+
+def unzip(li):
+  return tuple(map(list,zip(*li)))
 
 year = datetime.datetime.today().year
 
@@ -11,4 +15,8 @@ def getentry(s,year=datetime.datetime.today().year):
    entrytime = datetime.datetime.strptime(timestring,"%Y %b %d %X")
    return (entrytime,xs[4],xs[5])
 
-print(getentry(l[0]))
+print(l[:3])
+dd = dict(zip(["Time","Entry","Attr"],unzip(map(getentry,l[:3]))))
+data = pd.DataFrame(dd)
+print(data)
+#print(unzip(getentry(l[0])))
