d0d25673b830f8b14523fff7840c736e3d8294ff
Reiner Czerwinski <rczerwi@web.de>
2019-02-18 07:56:32 +0100

    plt einbinden

--- readlog.py
+++ readlog.py
@@ -1,29 +1,35 @@
 import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-path = "/var/log/"
-#path = ""
+import sys
 
-f = open(path+"syslog","r")
+#path = "/var/log/"
+path = ""
+
+f = open(path+"syslog", "r")
 l = f.readlines()
 
 def unzip(li):
-  return tuple(map(list,zip(*li)))
+  return tuple(map(list, zip(*li)))
 
 year = datetime.datetime.today().year
 
 def getentry(s,year=datetime.datetime.today().year):
-   xs=s.split(" ",5)
+   xs = s.split(" ", 5)
    timestring = str(year)+' '+' '.join(xs[0:3])
-   entrytime = datetime.datetime.strptime(timestring,"%Y %b %d %X")
-   return (entrytime,xs[4],xs[5])
+   entrytime = datetime.datetime.strptime(timestring, "%Y %b %d %X")
+   return (entrytime, xs[4], xs[5])
 
 #print(l[:3])
-dd = dict(zip(["Time","Tag","Attr"],unzip(map(getentry,l))))
+dd = dict(zip(["Time", "Tag", "Attr"], unzip(map(getentry, l))))
 data = pd.DataFrame(dd)
 
 print(data.head())
 #print(unzip(getentry(l[0])))
-print(data.groupby(['Time','Tag']).count())
+print(data.groupby(['Time', 'Tag']).count())
 #print(data.groupby(['Tag']).count())
 #print(data[data['Tag'].str.contains("CRON")])
+
+print (sys.argv)
